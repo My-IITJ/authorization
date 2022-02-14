df1fb85b88c59ea7f6ec12de710b5a9c66e5fc89
Swara <swarapatel1357@gmail.com>
2022-02-14 13:48:09 +0530

    added form templates

--- frontend/auth_frontend/src/App.js
+++ frontend/auth_frontend/src/App.js
@@ -1,10 +1,40 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import SignIn from "./Components/SignIn.js";
+import SignUp from "./Components/SignUp.js";
 
 function App() {
-  return (
+  return (<Router>
     <div className="App">
-      Hi
-    </div>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>home</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>SignIn</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>SignUp</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={SignIn} />
+            <Route path="/sign-in" component={SignIn} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
 }
 
---
+++ frontend/auth_frontend/src/Components/SignUp.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <form action='http://127.0.0.1:8000/api/signup/' method='post'>
+                <h3>Sign Up</h3>
+
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" placeholder="First name" name='first_name'/>
+                </div>
+
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" placeholder="Last name" name='last_name' />
+                </div>
+
+                <div className="form-group">
+                    <label>User name</label>
+                    <input type="text" className="form-control" placeholder="User name" name='username'/>
+                </div>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" name='email'/>
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" name='password'/>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <p className="forgot-password text-right">
+                    Already registered <a href="#">sign in?</a>
+                </p>
+            </form>
+        );
+    }
+}
